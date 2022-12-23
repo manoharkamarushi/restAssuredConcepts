200576832d2342f4058bd19496c6ec8deb6ad772
manok584 <manohar.kamarushi@gmail.com>
2022-12-23 19:57:33 +0530

    JsonPath parsing attributes

--- src/test/java/restAssured/CRUDAPI.java
+++ src/test/java/restAssured/CRUDAPI.java
@@ -7,8 +7,9 @@ import org.testng.annotations.Test;
 
 import files.Payloads;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
-public class Basics {
+public class CRUDAPI {
 
 @Test()
 public void verifyAddPlaceAPI(){
@@ -19,9 +20,17 @@ public void verifyAddPlaceAPI(){
 
 		// validate if Add Place API is workimg as expected 
 		RestAssured.baseURI="https://rahulshettyacademy.com";
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		String addPlaceResponse=given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
 		.body(Payloads.addPlace()).when().post("maps/api/place/add/json")
-		.then().log().all().assertThat().statusCode(201).body("scope",equalTo("APP")).header("Server", "Apache/2.4.41 (Ubuntu)");
+		.then().log().all().assertThat().statusCode(200).body("scope",equalTo("APP")).header("Server", "Apache/2.4.41 (Ubuntu)")
+		.extract().response().asString(); //extracting response and save it as string
+		
+		//extract response and want to parse it use jsonPath class
+		JsonPath json= new JsonPath(addPlaceResponse);
+		String place_id = json.get("place_id");
+		System.out.println(place_id);
+		
+		
 		
 		
 	}
