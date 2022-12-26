f44d5269e3518d44257987d1cd17b5a4336163de
manok584 <manohar.kamarushi@gmail.com>
2022-12-26 16:54:53 +0530

    readPayloadFromJsonFile

--- src/test/java/restAssured/LibraryAPI.java
+++ src/test/java/restAssured/LibraryAPI.java
@@ -2,6 +2,10 @@ package restAssured;
 
 import static org.hamcrest.Matchers.equalTo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -15,7 +19,7 @@ import static io.restassured.RestAssured.*;
 public class LibraryAPI {
 
 	
-	@Test(dataProvider = "multipleBooks")
+	@Test(dataProvider = "multipleBooks",enabled=false)
 	public void testAddLibraryAPI(String isbn, String asile) {
 		
 			RestAssured.baseURI="http://216.10.245.166";
@@ -41,4 +45,21 @@ public class LibraryAPI {
 		return new Object[][] {{"api2","261202"},{"api3","261203"},{"ap4","261204"}};
 	}
 	
+	
+	
+	@Test
+	public void readPayloadFromJsonFile() throws Exception {
+		
+		RestAssured.baseURI="http://216.10.245.166";
+		
+		String response= given().log().all().header("Content-Type","application/json").body(new String(Files.readAllBytes(Paths.get("./src/test/java/testData/addBook.json")))).
+		when().post("/Library/Addbook.php").
+		then().log().all().assertThat().statusCode(200).body("Msg",equalTo("successfully added")).extract().response().asString();
+		
+		JsonPath json = new JsonPath(response);
+		String bookID= json.get("ID");
+		System.out.println(bookID);
+		
+		
+	}
 }
